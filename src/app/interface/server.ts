716f48eaef9ce573865ee5616206386a1f265035
thteam <thteam47@gmail.com>
2021-12-14 01:23:00 +0700

    up code

---
+++ src/app/interface/server.ts
@@ -0,0 +1,11 @@
+
+export interface Server {
+  idServer: string;
+  serverName: string;
+  username: string;
+  password: string;
+  ip: string;
+  description: string;
+  status: string;
+  port: number;
+}
